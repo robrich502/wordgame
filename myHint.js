4ab628ee6b87c2fd6220a6beb9efd0aeb5f47b05
errinjohnson <errin@alchemymomentum.com>
2015-02-11 15:47:03 -0500

    add myFunc.js and Myhint.js to seperate behavior from content

--- myHint.js
+++ myHint.js
@@ -1,2 +1,45 @@
-myHint.js
-appending new file MyHint.js
+
+ // initialize our data store. our data store for this game happens to be an array built into the webpage.
+// in a real scenario, the data would come from calls to a database, or a web service.
+var secretWords = [
+    {
+    word: "kentucky",
+    hint: "A blue grass state."
+    },
+    {
+    word: "indiana",
+    hint: "Bobby Knight was basketball coach in this state."
+    },
+    {
+    word: "florida",
+    hint: "The sunshine state, known for it's oranges."
+    },
+    {
+    word: "tennessee",
+    hint: "Country musicians, line dancing, and home of Gatlinburg"
+    },
+    {
+    word: "virginia",
+    hint: "Home to the US Navy's 5th fleet and many other military bases."
+    },
+    {
+    word: "new york",
+    hint: "The big apple."
+    },
+    {
+    word: "georgia",
+    hint: "Famous for peaches."
+    },
+    {
+    word: "california",
+    hint: "Full of movie stars."
+    },
+    {
+    word: "ohio",
+    hint: "Buckeyes."
+    },
+    {
+    word: "texas",
+    hint: "Home of cowboys and cattle."
+    }
+    ];
